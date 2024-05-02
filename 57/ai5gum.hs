51e046fe2a852cf76d96139eddebb039f7419e4e
stazarzxy <d16174lhun73r@gmail.com>
2024-05-02 14:05:17 -0400

    upload 57 meow

---
+++ 57/ai5gum.hs
@@ -0,0 +1,144 @@
+-- // Universe 8y SansOfUndertale#9345 (UID: 414833316080910341) // --
+-- ---- ------ -------- ----------  ---------- -------- ------ ---- --
+import Data.Char  
+neq n q = toUpper n /= toUpper q         
+nem [] = []    
+nem (h:t) = h : nem (filter (neq h) t)          
+c = "Waffle, Reall wonderyful Do u ever dream about EATNG. waffle hnhng,, Sorrysorrysorry"             
+x = "grow showing who .? GROW SHOWING WHO?!"        
+s = nem [h | h <- c, elem h x]         
+       
+-- your soul evaporates now --
+lisabeth = "I just have to bridge this gap of understanding; please bear with me here!! ! ! !"
+holliday = "I rly rly just need to buy some time, so my friends can escape with their lives;"
+writing = "it does n t seem like this plan will work -- I am still trying very hard, though !!"
+really = "What the hell is a hatchery supposed to be, anyway? A  wise  Ard once told me!" 
+funny = "He had snowmelt magic powers also.  She could even summon torrential snowmelt!"       
+lmao = "                                                                                       "
+rue = "You walked along the bridge some years ago.  The path ahead narrows but it's fine;"
+me = "it does not split (you think).  The ground is marshy beneath, so very marshy where"
+i = "the bridge is planted.  You are surprised that it can bear this weight, but you're"
+am = "nearly to The Hatchery already.  You stop to pluck a rose, but it FUCKING PRICKS" 
+not = "your SOFT DELICATE SKIN and you CRY an DOWNPOUR like a SNOWMELT 's worth of TEARS"
+what = "oh by the POWERS ABOVE how could this HAPPEN to you? How HARSH a FATE imparted??"               
+looks = "                                                                                             "
+within = "DID YOU KNOW GUYS I HAVE A CON FES SION THOUGH I GUESS IT COULD BE PRETTY OBVIOUS H H H"
+eternal = "I LOVE BEARS ! I LOVE BEARS ! MEOW MEOW I MEOW FOR BEARS & & ! YES I MEAN THE ANIMAL !"
+darkness = "BUT LIKE ALSO GAY PERSON BEAR AA ! I WOULD SIT DOWN AND PLAY A HAND OF BRIDGE WITH"
+hatchery = "ONE AND TAKE THAT L.   DO YOU THINK AUSTIN POWERS WOULD BE A SNOWMELT BEAR OR NAH???"   
+bemoans = "                                                                                         "
+deaths = "Urgh, er, I don't know what came over me. Sorry about that. I guess, like, maybe I should"
+since = "apologize to the bear community as well? Just, like, bridge over troubled waters, clean "
+life = "slate. I might -- I still need to get somewhere. A hatchery I think. Gosh. I keep getting so"
+was = "distracted, like there's something keeping me away from myseBRRRRPRPRPRPPRAAAA PRESS F  TO "
+it = "TO PAY YOUR RESPOECTS TO MY BRAIN POWERS TROLOL LOL CALL ME OLAF THE WAY I SNOWMELT UP IN THERE"
+                                                                                        
+--thats what it feels like. I think. she never did get very far. or very close.
+--they're basically the same thing. the bridge was too long. and that was too much.
+--hm. bridge? that almost reminds me of something. or someone.
+--we evaporated?
+    
+that doesn't even make any sense like we thought maybe such lonely words could clear the path = putStrLn(the++'\n':words)
+but the path narrows even though it'll never split and lead us astray towards marshy death = what
+
+-- the bridge and the burden we bear on the way to the hatchery -- OOH. WHAT?! 
+-- POWERS MARSHY SNOWMELT WAFFLE MARSHY SNOWMELT POWERS WAFFLE SNOWMELT POWERS MARSHY WAF-
+                                                                                              
+--no no nonono no nono no nono no onno on nono noim so close oh please i can't
+--ok so um there's a bridge and the path narrows while we're still young. bear with me
+--BEAR WITH ME AUGhagh it keeps happening every single time i just try to remember
+--that the stupid path doesn't split and the path is marshy because the ground gets marshy
+--because it can't bear the weight of the stupid bridge or something
+--and like somewhere there's the
+--there's the haTCHERY I'M SO CLOSE.
+
+--but
+--i just can't remember the    wise    Ard and their fortune-telling snowmelt powers
+                                                                                             
+is = "Rapids ?  Nobody said anything about rapids i don't think.  Rapids sound dangerous !"
+sun = "Heck, i might need a backup clone or something !  Heh, wdym we're not doing sci-fi ?"
+soft = "Well, here's a better question.  Do you wanna have a bad time ?  'Cause if you take"
+light = "another step forward, well, you're REALLY not going to like what happens next. "
+bridge = "Her singular, eightfold eye narrows and you almost feel your mind split into two !"
+cries = "What ?  I'm not doing cringe roleplay, you're doing cringe roleplay !  Hmmm, something"
+pain = "feels MARSHY unusual.  Is this BRIDGE real, after all ?  guys i want to hug a bear AND"
+for = "AND SANS UNDER THE BRIDck wall.  Nobody said anything about HATCHERY the bears THE HATCHERY"    
+my = "IS HERE OHh and austin powers the snowmelt bear can join us too if you dont mind hehe"
+tragedy forever never to be seen again because without your love we can't remember why we're here =c 
+                                                                                                    
+--  ||||||||||  ##################  ||||||||||  -- ------------- ######### -----------
+--  ||||||  ####  ||||||||||||||  ####  ||||||  -- ------ ^ - ###|||||## -- , --------
+--  ||||  ##  ||||||||||||||||||||||  ##  ||||  -- ---- ||:##||||||||||||||#:|==> ----
+--  ||||  ##  |  tHaNkS_t0 OlIvIa  |  ##  ||||  -- --- -,:-#||#||||||||||||#--# ------
+--  ||  ##  ||||||||||||||||||||||||||| ##  ||  -- -- ##||-==|||||||||#||#==-|## -----
+--  ||  ##  ||||||||||||||||| ###### || ##  ||  -- -- ##|||##=||||||||||##|#||||## ---
+--  ||  ##  ||||||||||||||||| ###### || ##  ||  -- -- ##|#||#::*######||##||#|#|## ---
+--  ||  ##  | ###### |  ##  | ###### || ##  ||  -- - #|##|||#***##,,,##**##||#|||||# -
+--  ||||  ##  ||||||  ######  ||||||  ##  ||||  -- - #||||#|#-   :#+#,  ,=#||||######
+--  ||  ####  ##  ||||||||||||||  ##  ####  ||  -- - #||||#|######,,,####|:#||## -----
+--  ||  ##    ######################    ##  ||  -- -- ##|||###-|||***|**+#||||## -----
+--  ||  ##  ||  ##  ##  ##  ##  ##  ||  ##  ||  -- --- ##|||#|##*+---+*|||||||## -----
+--  ||||  ####    ##############    ####  ||||  -- --- ##|||||||##:::#||||||||## -----
+--  ||  ##########  ||||||||||  ##########  ||  -- #  ##|||||#############||||||##  #
+--  | ##  ##############################  ## |  -- ###||##||#@@#########@##||##|||###
+--  ####  ##    ##  ||  ##  ||  ##    ##  ####  --  #|||||||#@@@######@@@##||||||||# -
+--  ##  ||  ##    ######  ######    ##  ||  ##  -- --- ##||#@@##|**#*#|##@@#|||## ----
+-- #    ####  ######    ##    ######  ####    # -- -- ###||#@@##|##*#*|##@@#|||### ---
+-- #  ||||  ##    ##  ||||||  ##    ##  ||||  # -- -- ##|||#@@##|##*#*|###@#||||## ---
+-- #  ||||||  ##  ##  ||||||  ##  ##  ||||||  # -- -- ##|||#@#########|###|#||||## ---
+--  ##  ||  ##  | ####  ||  #### |  ##  ||  ##  -- --- ##||############*####|||## ----
+--  | ####  ##  | ##  ||||||  ## |  ##  #### |  -- ---- ##|#:#||||||||||#|:#||## -----
+--  ||  ######  | ############## |  ######  ||  -- --- ##|||##|||#####|||##||||## ----
+--  ||||  ####  | ############## |  ####  ||||  -- ---- #### #|||# - #|||# ##### -----
+--  ||  ##################################  ||  -- --------- #|||# - #|||# -----------
+--  ||  ################  ################  ||  -- --------- #|||# - #|||# -----------
+--  ||||  ############  ||  ############  ||||  -- --------- #|||# - #|||# -----------
+--  | ######  ||||  ##  ||  ##  ||||| ###### |  -- --------- #**## - #**## -----------
+--  | ##  ||||||  ####  ||  ####  ||||||  ## |  -- --------- #|||# - #|||# -----------                        
+--  ||  ##########              ##########  ||  --                                                           
+                                                                                                              
+main = that was really funny since i am really what looks within darkness (but soft
+ what light within my hatchery cries it is my pain my life my sun) for it s me -- it was always supposed to be me
+
+-- ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ --
+--_________________________________________________________________________--
+--___ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ___________________________________________________--
+--___ Dearest Lisabeth, ___________________________________________________--
+--_________________________________________________________________________--
+--______ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ____________--
+--______ Would it mean anything at all to say that I am sorry? ____________--
+--_________________________________________________________________________--
+--______ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ __--
+--______ I am just so terrified of what is to come.  It would be so nice __--
+--_________________________________________________________________________--
+--___ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ____________________--
+--___ just to mmet with you one last time.  Thank you. ____________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_____________________________________________ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ __________--
+--_____________________________________________ Sincerely yours, __________--
+--_________________________________________________________________________--
+--________________________________________________ ¯¯¯¯¯¯¯¯¯¯¯¯ ___________--
+--________________________________________________ Bio Holliday ___________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+--_________________________________________________________________________--
+-- ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ ¦ --
+
+
+
+
+
+-- i was the snowmelt all along --
